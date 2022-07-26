48b5e1fe907d287d33b9c4a13f82603b56fc64a8
Trittton <gatsyuk@gmail.com>
2022-07-26 19:00:44 +0300

    .

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
