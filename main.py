817adb438d87c724d863e024ba5174967df4190d
Trittton <gatsyuk@gmail.com>
2022-07-26 19:03:31 +0300

    .

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv('TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
@@ -13,7 +13,7 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{BOT_TOKEN}'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
