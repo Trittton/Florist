9d543de832a9e7c160c67da2ca134dd84eb4edfb
Trittton <gatsyuk@gmail.com>
2022-07-26 22:47:58 +0300

    .

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import logging
-
+from aiogram import Bot
 from aiogram import types
 from aiogram import Dispatcher
+from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
-from config import bot, dp, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 from db import database
+import os
+
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.getenv('PORT', default=8000))
+DB_URL = os.getenv('DATABASE_URL')
 
 
 async def on_startup(dispatcher: Dispatcher) -> None:
